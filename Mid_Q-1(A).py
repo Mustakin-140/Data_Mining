cd3797d8d4f324f4117c96d979adf3621d065aa5
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2022-12-02 20:24:49 +0600

    Add files via upload

---
+++ Mid_Q-1(A).py
@@ -0,0 +1,10 @@
+marks = int(input("Enter your marks: "))
+
+if marks >=71 and marks<=100:
+    print("Your grade is : A")
+elif marks >= 56 and marks <=70:
+    print("Your grade is : B")
+elif marks >= 31 and marks <= 55:
+    print("Your grade is : C")
+elif marks >= 0 and marks <= 30:
+    print("Your grade is : F")
